a838570e8ff8e997414f028251b1a39e08d58d01
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:34 -0600

    Add RevampCodingStandardRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Ghostwriter/CodingStandard/RevampCodingStandardRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Ghostwriter\CodingStandard;
+
+use Ghostwriter\Revamp\Rule\Ghostwriter\CodingStandard\RevampCodingStandardRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampCodingStandardRector::class)]
+final class RevampCodingStandardRectorTest extends AbstractTestCase
+{
+}
