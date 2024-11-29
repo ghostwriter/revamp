c91de82bc4699fd9dae289e144e1c2b6e56c246c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:13 -0600

    Add config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/fixture/Rule/Mezzio/MezzioFlash/RevampMezzioFlashRector/config.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()->withRules(
+    [\Ghostwriter\Revamp\Rule\Mezzio\MezzioFlash\RevampMezzioFlashRector::class]
+);
