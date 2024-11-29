fe6abebfa1723a689edbae7a3703cd8e7d641329
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:35 -0600

    Add RevampEventDispatcherRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Ghostwriter/EventDispatcher/RevampEventDispatcherRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Ghostwriter\EventDispatcher;
+
+use Ghostwriter\Revamp\Rule\Ghostwriter\EventDispatcher\RevampEventDispatcherRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampEventDispatcherRector::class)]
+final class RevampEventDispatcherRectorTest extends AbstractTestCase
+{
+}
