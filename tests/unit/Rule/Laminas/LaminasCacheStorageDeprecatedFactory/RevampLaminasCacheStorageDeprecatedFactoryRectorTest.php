099b393c3990fedf13a3249f6d0706572f766493
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:38 -0600

    Add RevampLaminasCacheStorageDeprecatedFactoryRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasCacheStorageDeprecatedFactory/RevampLaminasCacheStorageDeprecatedFactoryRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasCacheStorageDeprecatedFactory;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasCacheStorageDeprecatedFactory\RevampLaminasCacheStorageDeprecatedFactoryRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasCacheStorageDeprecatedFactoryRector::class)]
+final class RevampLaminasCacheStorageDeprecatedFactoryRectorTest extends AbstractTestCase
+{
+}
