98de752c258164b6e9d0a2f8a5973d3c85c01e93
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-29 07:43:32 -0600

    Move config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Rule/PHP/Core/SortUseStatementsAlphabeticallyRector/config.php
+++ tests/fixture/Rule/PHP/Core/SortUseStatementsAlphabeticallyRector/config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Ghostwriter\Revamp\Rule\SortUseStatementsAlphabeticallyRector;
+use Ghostwriter\Revamp\Rule\PHP\Core\SortUseStatementsAlphabeticallyRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()->withRules([SortUseStatementsAlphabeticallyRector::class]);
