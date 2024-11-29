80dffcf4b088ede59d01f4b8d5c32d8610527518
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:35 -0600

    Add RevampHandrailRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Ghostwriter/Handrail/RevampHandrailRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Ghostwriter\Handrail;
+
+use Ghostwriter\Revamp\Rule\Ghostwriter\Handrail\RevampHandrailRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampHandrailRector::class)]
+final class RevampHandrailRectorTest extends AbstractTestCase
+{
+}
