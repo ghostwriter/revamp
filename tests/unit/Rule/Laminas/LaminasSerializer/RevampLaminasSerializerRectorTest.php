6aa9147838af05ab74ecf79ecafee1a2183e7f52
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:48 -0600

    Add RevampLaminasSerializerRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasSerializer/RevampLaminasSerializerRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasSerializer;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasSerializer\RevampLaminasSerializerRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasSerializerRector::class)]
+final class RevampLaminasSerializerRectorTest extends AbstractTestCase
+{
+}
