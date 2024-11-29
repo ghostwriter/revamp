ab199a63b11f78b1d9a4dc366727bdb98a25d5c5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:52:00 -0600

    Add ShouldReceiveToExpectsRector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Rule/Mockery/ShouldReceiveToExpectsRector.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Revamp\Rule\Mockery;
+
+use Ghostwriter\Revamp\AbstractRevampRector;
+use Ghostwriter\RevampTests\Rule\Mockery\ShouldReceiveToExpectsRectorTest;
+
+/**
+ * @see ShouldReceiveToExpectsRectorTest
+ */
+final class ShouldReceiveToExpectsRector extends AbstractRevampRector
+{
+}
