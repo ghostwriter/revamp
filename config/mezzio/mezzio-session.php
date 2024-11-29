3127ffbfda6d6ed1ebb1ffb0062a4b7499773227
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:31 -0600

    Add mezzio-session.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/mezzio/mezzio-session.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([\Ghostwriter\Revamp\Rule\Mezzio\MezzioSession\RevampMezzioSessionRector::class])
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Mezzio\MezzioSession\MezzioSessionSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
