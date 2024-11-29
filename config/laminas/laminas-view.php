1e352c2634599154ba0586e7d1e9b373eec659c3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:28 -0600

    Add laminas-view.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/laminas/laminas-view.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([\Ghostwriter\Revamp\Rule\Laminas\LaminasView\RevampLaminasViewRector::class])
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Laminas\LaminasView\LaminasViewSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
