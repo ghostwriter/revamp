540763bf834149c4962dca295ec6259763423e9f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:23 -0600

    Add laminas-escaper.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/laminas/laminas-escaper.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([\Ghostwriter\Revamp\Rule\Laminas\LaminasEscaper\RevampLaminasEscaperRector::class])
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Laminas\LaminasEscaper\LaminasEscaperSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
