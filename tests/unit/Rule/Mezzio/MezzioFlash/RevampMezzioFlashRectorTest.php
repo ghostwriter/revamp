4503d3e78cdb510362a6a6b56fcdf7bea1982c3c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:52 -0600

    Add RevampMezzioFlashRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioFlash/RevampMezzioFlashRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioFlash;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioFlash\RevampMezzioFlashRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioFlashRector::class)]
+final class RevampMezzioFlashRectorTest extends AbstractTestCase
+{
+}
