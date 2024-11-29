7364daffa37ddb27faa86f425feefeef6a3e63f9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:52 -0600

    Add RevampMezzioHelpersRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioHelpers/RevampMezzioHelpersRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioHelpers;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioHelpers\RevampMezzioHelpersRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioHelpersRector::class)]
+final class RevampMezzioHelpersRectorTest extends AbstractTestCase
+{
+}
