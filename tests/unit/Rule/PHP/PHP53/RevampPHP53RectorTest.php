9a78102f9cef2423c57f6956f5d1ad88baa20bf9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:55 -0600

    Add RevampPHP53RectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/PHP/PHP53/RevampPHP53RectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\PHP\PHP53;
+
+use Ghostwriter\Revamp\Rule\PHP\PHP53\RevampPHP53Rector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampPHP53Rector::class)]
+final class RevampPHP53RectorTest extends AbstractTestCase
+{
+}
