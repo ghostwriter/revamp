9cd5c7dcaa916d30eefc5ac583b5a1097f3d0b4e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:54 -0600

    Add ProphecyToMockeryRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mockery/ProphecyToMockeryRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mockery;
+
+use Ghostwriter\Revamp\Rule\Mockery\ProphecyToMockeryRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(ProphecyToMockeryRector::class)]
+final class ProphecyToMockeryRectorTest extends AbstractTestCase
+{
+}
