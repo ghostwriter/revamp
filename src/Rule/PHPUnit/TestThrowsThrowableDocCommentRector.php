08ef27a486c69a593703ced3b1af38e60d64fc04
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:52:03 -0600

    Add TestThrowsThrowableDocCommentRector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Rule/PHPUnit/TestThrowsThrowableDocCommentRector.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Revamp\Rule\PHPUnit;
+
+use Ghostwriter\Revamp\AbstractRevampRector;
+use Ghostwriter\RevampTests\Rule\PHPUnit\TestThrowsThrowableDocCommentRectorTest;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassMethod;
+
+/**
+ * @see TestThrowsThrowableDocCommentRectorTest
+ */
+final class TestThrowsThrowableDocCommentRector extends AbstractRevampRector
+{
+    /**
+     * @return array<class-string<ClassMethod>>
+     */
+    #[\Override]
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    #[\Override]
+    public function refactor(Node $node): ?Node
+    {
+        return match (true) {
+            $node instanceof ClassMethod => $this->refactorClassMethod($node),
+            default => null,
+        };
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    private function refactorClassMethod(ClassMethod $classMethod): ?ClassMethod
+    {
+        return match (true) {
+            $this->isPHPUnitTestClassMethod($classMethod) => $this->addThrowsThrowableDocComment($classMethod),
+            default => null,
+        };
+    }
+}
