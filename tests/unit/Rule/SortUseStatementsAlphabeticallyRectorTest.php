f62dbcf8122789d1413811bafa352bf00b3ac40a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:54:01 -0600

    Add SortUseStatementsAlphabeticallyRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/SortUseStatementsAlphabeticallyRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule;
+
+use Ghostwriter\Revamp\Rule\SortUseStatementsAlphabeticallyRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(SortUseStatementsAlphabeticallyRector::class)]
+final class SortUseStatementsAlphabeticallyRectorTest extends AbstractTestCase
+{
+}
