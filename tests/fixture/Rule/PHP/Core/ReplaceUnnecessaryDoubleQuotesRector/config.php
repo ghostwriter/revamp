368e0ddce53eb8134c49c0243182d09fcf39b9c4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-29 07:43:32 -0600

    Move config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Rule/PHP/Core/ReplaceUnnecessaryDoubleQuotesRector/config.php
+++ tests/fixture/Rule/PHP/Core/ReplaceUnnecessaryDoubleQuotesRector/config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Ghostwriter\Revamp\Rule\ReplaceUnnecessaryDoubleQuotesRector;
+use Ghostwriter\Revamp\Rule\PHP\Core\ReplaceUnnecessaryDoubleQuotesRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()->withRules([ReplaceUnnecessaryDoubleQuotesRector::class]);
