e7e720378578118bc9d47c9b141384f55fc81ca9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:54:02 -0600

    Add RevampAssertRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Webmozart/Assert/RevampAssertRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Webmozart\Assert;
+
+use Ghostwriter\Revamp\Rule\Webmozart\Assert\RevampAssertRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampAssertRector::class)]
+final class RevampAssertRectorTest extends AbstractTestCase
+{
+}
