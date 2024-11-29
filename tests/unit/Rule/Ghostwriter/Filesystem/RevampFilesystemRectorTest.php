50ee797306525ed1f2f1376cf999c6395b240138
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:35 -0600

    Add RevampFilesystemRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Ghostwriter/Filesystem/RevampFilesystemRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Ghostwriter\Filesystem;
+
+use Ghostwriter\Revamp\Rule\Ghostwriter\Filesystem\RevampFilesystemRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampFilesystemRector::class)]
+final class RevampFilesystemRectorTest extends AbstractTestCase
+{
+}
