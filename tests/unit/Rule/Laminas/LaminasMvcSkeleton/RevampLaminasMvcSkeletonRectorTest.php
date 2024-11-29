4e04a68f8d9b7d9acf92c9725bd76a0585ad81d8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:47 -0600

    Add RevampLaminasMvcSkeletonRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasMvcSkeleton/RevampLaminasMvcSkeletonRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasMvcSkeleton;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasMvcSkeleton\RevampLaminasMvcSkeletonRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasMvcSkeletonRector::class)]
+final class RevampLaminasMvcSkeletonRectorTest extends AbstractTestCase
+{
+}
