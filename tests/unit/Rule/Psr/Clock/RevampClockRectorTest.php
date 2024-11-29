c9c0a76dd82d01607c9b5566226bf3b664750b5e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:58 -0600

    Add RevampClockRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Psr/Clock/RevampClockRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Psr\Clock;
+
+use Ghostwriter\Revamp\Rule\Psr\Clock\RevampClockRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampClockRector::class)]
+final class RevampClockRectorTest extends AbstractTestCase
+{
+}
