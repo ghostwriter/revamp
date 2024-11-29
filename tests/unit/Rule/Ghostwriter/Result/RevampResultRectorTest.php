bb279a052488ef27c7801c0fb893c7db4b852a52
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:36 -0600

    Add RevampResultRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Ghostwriter/Result/RevampResultRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Ghostwriter\Result;
+
+use Ghostwriter\Revamp\Rule\Ghostwriter\Result\RevampResultRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampResultRector::class)]
+final class RevampResultRectorTest extends AbstractTestCase
+{
+}
