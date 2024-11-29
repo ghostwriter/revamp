f8a7d2ae781689d04fa47f1981ecb8499690df62
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:47 -0600

    Add RevampLaminasPaginatorAdapterLaminasdbRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasPaginatorAdapterLaminasdb/RevampLaminasPaginatorAdapterLaminasdbRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasPaginatorAdapterLaminasdb;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasPaginatorAdapterLaminasdb\RevampLaminasPaginatorAdapterLaminasdbRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasPaginatorAdapterLaminasdbRector::class)]
+final class RevampLaminasPaginatorAdapterLaminasdbRectorTest extends AbstractTestCase
+{
+}
