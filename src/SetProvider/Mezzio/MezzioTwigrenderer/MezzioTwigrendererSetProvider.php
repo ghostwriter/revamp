1d060ea8d0a5bee2b396a8ed34ed99c73a94ff50
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:52:27 -0600

    Add MezzioTwigrendererSetProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/SetProvider/Mezzio/MezzioTwigrenderer/MezzioTwigrendererSetProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Revamp\SetProvider\Mezzio\MezzioTwigrenderer;
+
+use Rector\Set\Contract\SetInterface;
+use Rector\Set\Contract\SetProviderInterface;
+use Rector\Set\ValueObject\ComposerTriggeredSet;
+use Rector\Set\ValueObject\Set;
+
+final readonly class MezzioTwigrendererSetProvider implements SetProviderInterface
+{
+    /**
+     * @return SetInterface[]
+     */
+    #[\Override]
+    public function provide(): array
+    {
+        return [
+            new ComposerTriggeredSet('group_name', 'package_name', '1.0', __DIR__ . 'set_file_path.php'),
+            new Set('group_name', 'set_name', __DIR__ . 'set_file_path.php'),
+        ];
+    }
+}
