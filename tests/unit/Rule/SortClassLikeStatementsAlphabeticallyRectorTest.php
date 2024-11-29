c2b27a4c72701bb25015e7e48901b478bc19da14
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:54:01 -0600

    Add SortClassLikeStatementsAlphabeticallyRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/SortClassLikeStatementsAlphabeticallyRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule;
+
+use Ghostwriter\Revamp\Rule\SortClassLikeStatementsAlphabeticallyRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(SortClassLikeStatementsAlphabeticallyRector::class)]
+final class SortClassLikeStatementsAlphabeticallyRectorTest extends AbstractTestCase
+{
+}
