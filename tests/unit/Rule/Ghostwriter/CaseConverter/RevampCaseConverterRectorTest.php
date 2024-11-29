bba28b7950cb9125517c3165dcd513c790240592
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:34 -0600

    Add RevampCaseConverterRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Ghostwriter/CaseConverter/RevampCaseConverterRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Ghostwriter\CaseConverter;
+
+use Ghostwriter\Revamp\Rule\Ghostwriter\CaseConverter\RevampCaseConverterRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampCaseConverterRector::class)]
+final class RevampCaseConverterRectorTest extends AbstractTestCase
+{
+}
