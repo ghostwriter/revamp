93c0ab72da0c9e9582652c2b8263965bfa1af3da
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-29 07:43:31 -0600

    Update laminas-config-aggregator-parameters.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/laminas/laminas-config-aggregator-parameters.php
+++ config/laminas/laminas-config-aggregator-parameters.php
@@ -6,7 +6,9 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withRules(
-        [\Ghostwriter\Revamp\Rule\Laminas\LaminasConfigAggregatorParameters\RevampLaminasConfigAggregatorParametersRector::class]
+        [
+            \Ghostwriter\Revamp\Rule\Laminas\LaminasConfigAggregatorParameters\RevampLaminasConfigAggregatorParametersRector::class,
+        ]
     )
 //    ->withSetProviders(
 //      // once this method is accepted, uncomment this line
