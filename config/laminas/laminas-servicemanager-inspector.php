f5b6a70e320724bcf4b27c4424dd6edb7b807235
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-29 07:43:31 -0600

    Update laminas-servicemanager-inspector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/laminas/laminas-servicemanager-inspector.php
+++ config/laminas/laminas-servicemanager-inspector.php
@@ -6,7 +6,9 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withRules(
-        [\Ghostwriter\Revamp\Rule\Laminas\LaminasServicemanagerInspector\RevampLaminasServicemanagerInspectorRector::class]
+        [
+            \Ghostwriter\Revamp\Rule\Laminas\LaminasServicemanagerInspector\RevampLaminasServicemanagerInspectorRector::class,
+        ]
     )
 //    ->withSetProviders(
 //      // once this method is accepted, uncomment this line
