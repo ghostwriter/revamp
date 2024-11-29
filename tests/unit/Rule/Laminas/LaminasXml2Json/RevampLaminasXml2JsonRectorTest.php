2b915d67f90feeba69d37f38e5658df081305555
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:50 -0600

    Add RevampLaminasXml2JsonRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasXml2Json/RevampLaminasXml2JsonRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasXml2Json;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasXml2Json\RevampLaminasXml2JsonRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasXml2JsonRector::class)]
+final class RevampLaminasXml2JsonRectorTest extends AbstractTestCase
+{
+}
