269f5910c7cf7ab3528ec14c421b9cdaa65748a2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:52 -0600

    Add RevampMezzioAuthorizationRbacRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioAuthorizationRbac/RevampMezzioAuthorizationRbacRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioAuthorizationRbac;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioAuthorizationRbac\RevampMezzioAuthorizationRbacRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioAuthorizationRbacRector::class)]
+final class RevampMezzioAuthorizationRbacRectorTest extends AbstractTestCase
+{
+}
