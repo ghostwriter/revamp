7dac104cc04d6443648e4861cc18805723cc67ce
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:35 -0600

    Add RevampGfmRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Ghostwriter/Gfm/RevampGfmRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Ghostwriter\Gfm;
+
+use Ghostwriter\Revamp\Rule\Ghostwriter\Gfm\RevampGfmRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampGfmRector::class)]
+final class RevampGfmRectorTest extends AbstractTestCase
+{
+}
