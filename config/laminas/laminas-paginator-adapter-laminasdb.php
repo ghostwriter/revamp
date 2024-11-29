939df303bbad023287f15ee4feb9a18d98224177
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:26 -0600

    Add laminas-paginator-adapter-laminasdb.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/laminas/laminas-paginator-adapter-laminasdb.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules(
+        [\Ghostwriter\Revamp\Rule\Laminas\LaminasPaginatorAdapterLaminasdb\RevampLaminasPaginatorAdapterLaminasdbRector::class]
+    )
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Laminas\LaminasPaginatorAdapterLaminasdb\LaminasPaginatorAdapterLaminasdbSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
