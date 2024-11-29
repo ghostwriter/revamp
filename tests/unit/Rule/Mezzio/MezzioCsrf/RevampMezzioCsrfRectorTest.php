5c1d7bfafeccb1a9c5d08430dddf0ea5b600ea9a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:52 -0600

    Add RevampMezzioCsrfRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioCsrf/RevampMezzioCsrfRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioCsrf;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioCsrf\RevampMezzioCsrfRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioCsrfRector::class)]
+final class RevampMezzioCsrfRectorTest extends AbstractTestCase
+{
+}
