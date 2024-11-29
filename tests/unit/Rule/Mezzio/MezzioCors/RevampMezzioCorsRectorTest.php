707cc65350f0c615adee8234c7116163826ed5d2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:52 -0600

    Add RevampMezzioCorsRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioCors/RevampMezzioCorsRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioCors;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioCors\RevampMezzioCorsRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioCorsRector::class)]
+final class RevampMezzioCorsRectorTest extends AbstractTestCase
+{
+}
