050bb45e1fb9c0f60be66352b941e15d4a3df202
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:46 -0600

    Add RevampLaminasMvcPluginIdentityRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasMvcPluginIdentity/RevampLaminasMvcPluginIdentityRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasMvcPluginIdentity;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasMvcPluginIdentity\RevampLaminasMvcPluginIdentityRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasMvcPluginIdentityRector::class)]
+final class RevampLaminasMvcPluginIdentityRectorTest extends AbstractTestCase
+{
+}
