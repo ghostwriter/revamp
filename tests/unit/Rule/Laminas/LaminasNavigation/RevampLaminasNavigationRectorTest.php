f143bfe8dd5007994685a2e3d3ecebe6ff62d54b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:47 -0600

    Add RevampLaminasNavigationRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasNavigation/RevampLaminasNavigationRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasNavigation;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasNavigation\RevampLaminasNavigationRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasNavigationRector::class)]
+final class RevampLaminasNavigationRectorTest extends AbstractTestCase
+{
+}
