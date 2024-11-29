3ab898ed6aef873a49cacc06b8d04db2bcd80ea3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:20 -0600

    Add laminas-cache-storage-adapter-benchmark.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/laminas/laminas-cache-storage-adapter-benchmark.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules(
+        [\Ghostwriter\Revamp\Rule\Laminas\LaminasCacheStorageAdapterBenchmark\RevampLaminasCacheStorageAdapterBenchmarkRector::class]
+    )
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Laminas\LaminasCacheStorageAdapterBenchmark\LaminasCacheStorageAdapterBenchmarkSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
