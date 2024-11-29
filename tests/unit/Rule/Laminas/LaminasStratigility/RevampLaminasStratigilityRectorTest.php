ac8cdbf02fad9e29c8981391a524c2f3d7da5674
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:49 -0600

    Add RevampLaminasStratigilityRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasStratigility/RevampLaminasStratigilityRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasStratigility;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasStratigility\RevampLaminasStratigilityRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasStratigilityRector::class)]
+final class RevampLaminasStratigilityRectorTest extends AbstractTestCase
+{
+}
