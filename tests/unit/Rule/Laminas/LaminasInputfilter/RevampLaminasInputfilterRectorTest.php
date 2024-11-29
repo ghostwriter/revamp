6e65cc8196011b0acf3e795ff9bd93c840a27c46
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:43 -0600

    Add RevampLaminasInputfilterRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasInputfilter/RevampLaminasInputfilterRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasInputfilter;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasInputfilter\RevampLaminasInputfilterRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasInputfilterRector::class)]
+final class RevampLaminasInputfilterRectorTest extends AbstractTestCase
+{
+}
