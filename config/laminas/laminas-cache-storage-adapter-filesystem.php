7747cebf3b7f788e0e4a4d10eafe92a27d6e7ee5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-29 07:43:30 -0600

    Update laminas-cache-storage-adapter-filesystem.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/laminas/laminas-cache-storage-adapter-filesystem.php
+++ config/laminas/laminas-cache-storage-adapter-filesystem.php
@@ -6,7 +6,9 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withRules(
-        [\Ghostwriter\Revamp\Rule\Laminas\LaminasCacheStorageAdapterFilesystem\RevampLaminasCacheStorageAdapterFilesystemRector::class]
+        [
+            \Ghostwriter\Revamp\Rule\Laminas\LaminasCacheStorageAdapterFilesystem\RevampLaminasCacheStorageAdapterFilesystemRector::class,
+        ]
     )
 //    ->withSetProviders(
 //      // once this method is accepted, uncomment this line
