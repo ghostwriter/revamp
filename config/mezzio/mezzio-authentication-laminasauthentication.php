1a15321c22da5912bc573d105666f0bcf2f0f1d3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:28 -0600

    Add mezzio-authentication-laminasauthentication.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/mezzio/mezzio-authentication-laminasauthentication.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules(
+        [\Ghostwriter\Revamp\Rule\Mezzio\MezzioAuthenticationLaminasauthentication\RevampMezzioAuthenticationLaminasauthenticationRector::class]
+    )
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Mezzio\MezzioAuthenticationLaminasauthentication\MezzioAuthenticationLaminasauthenticationSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
