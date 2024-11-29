dc1a91e9caef9f0bc7a8a2d4beb46126a7178459
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:55 -0600

    Add ShouldReceiveToExpectsRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mockery/ShouldReceiveToExpectsRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mockery;
+
+use Ghostwriter\Revamp\Rule\Mockery\ShouldReceiveToExpectsRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(ShouldReceiveToExpectsRector::class)]
+final class ShouldReceiveToExpectsRectorTest extends AbstractTestCase
+{
+}
