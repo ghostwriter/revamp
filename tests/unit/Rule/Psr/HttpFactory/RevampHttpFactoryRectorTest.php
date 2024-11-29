95fec0c586e194d43654b97454e7471fb145fc98
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:59 -0600

    Add RevampHttpFactoryRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Psr/HttpFactory/RevampHttpFactoryRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Psr\HttpFactory;
+
+use Ghostwriter\Revamp\Rule\Psr\HttpFactory\RevampHttpFactoryRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampHttpFactoryRector::class)]
+final class RevampHttpFactoryRectorTest extends AbstractTestCase
+{
+}
