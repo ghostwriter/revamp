bdba02ded1f05a7acaeee3af612a86e797c0352e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:33 -0600

    Add RevampComposerRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Composer/Composer/RevampComposerRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Composer\Composer;
+
+use Ghostwriter\Revamp\Rule\Composer\Composer\RevampComposerRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampComposerRector::class)]
+final class RevampComposerRectorTest extends AbstractTestCase
+{
+}
