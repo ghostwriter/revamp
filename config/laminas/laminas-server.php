6d715a4224928dbea946b7b4fa4402f46422fb87
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:27 -0600

    Add laminas-server.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/laminas/laminas-server.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([\Ghostwriter\Revamp\Rule\Laminas\LaminasServer\RevampLaminasServerRector::class])
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Laminas\LaminasServer\LaminasServerSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
