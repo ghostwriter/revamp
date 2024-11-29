fff7306d8881a35f88dbbc011d6b2c068790f265
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:39 -0600

    Add RevampLaminasDeveloperToolsRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasDeveloperTools/RevampLaminasDeveloperToolsRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasDeveloperTools;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasDeveloperTools\RevampLaminasDeveloperToolsRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasDeveloperToolsRector::class)]
+final class RevampLaminasDeveloperToolsRectorTest extends AbstractTestCase
+{
+}
