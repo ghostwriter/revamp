ff445e53e7dcbfe8d626a993a9ec49db029723f6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:53 -0600

    Add RevampMezzioSessionRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioSession/RevampMezzioSessionRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioSession;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioSession\RevampMezzioSessionRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioSessionRector::class)]
+final class RevampMezzioSessionRectorTest extends AbstractTestCase
+{
+}
