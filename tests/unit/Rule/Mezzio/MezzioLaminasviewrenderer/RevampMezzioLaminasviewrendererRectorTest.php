d25552552280f15c33ea06323725ab2f70049f17
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:52 -0600

    Add RevampMezzioLaminasviewrendererRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioLaminasviewrenderer/RevampMezzioLaminasviewrendererRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioLaminasviewrenderer;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioLaminasviewrenderer\RevampMezzioLaminasviewrendererRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioLaminasviewrendererRector::class)]
+final class RevampMezzioLaminasviewrendererRectorTest extends AbstractTestCase
+{
+}
