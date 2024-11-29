4339865a38d6d3a93b43f4daface3f8c27a2715e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:27 -0600

    Add laminas-router.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/laminas/laminas-router.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([\Ghostwriter\Revamp\Rule\Laminas\LaminasRouter\RevampLaminasRouterRector::class])
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Laminas\LaminasRouter\LaminasRouterSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
