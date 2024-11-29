da75b9e7481b79878cb0e5c97c55c113c1b96f50
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:58 -0600

    Add RevampCacheRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Psr/Cache/RevampCacheRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Psr\Cache;
+
+use Ghostwriter\Revamp\Rule\Psr\Cache\RevampCacheRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampCacheRector::class)]
+final class RevampCacheRectorTest extends AbstractTestCase
+{
+}
