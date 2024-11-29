c836b479c4bdc5d789bebed6ef120cb4d8c93348
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:06 -0600

    Add config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/fixture/Rule/Laminas/LaminasTranslator/RevampLaminasTranslatorRector/config.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()->withRules(
+    [\Ghostwriter\Revamp\Rule\Laminas\LaminasTranslator\RevampLaminasTranslatorRector::class]
+);
