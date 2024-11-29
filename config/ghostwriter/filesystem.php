451bccb86620c7d1ed07beee2b895fdb307f3906
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:17 -0600

    Add filesystem.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/ghostwriter/filesystem.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([\Ghostwriter\Revamp\Rule\Ghostwriter\Filesystem\RevampFilesystemRector::class])
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Ghostwriter\Filesystem\FilesystemSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
