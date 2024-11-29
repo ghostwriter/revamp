f051af36a2e7273d6f54d61ee28cc9f4d95aacd1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:36 -0600

    Add RevampPsalmPluginRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Ghostwriter/PsalmPlugin/RevampPsalmPluginRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Ghostwriter\PsalmPlugin;
+
+use Ghostwriter\Revamp\Rule\Ghostwriter\PsalmPlugin\RevampPsalmPluginRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampPsalmPluginRector::class)]
+final class RevampPsalmPluginRectorTest extends AbstractTestCase
+{
+}
