9330748274a48170c1690333e6b991b6ec5f879e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:59 -0600

    Add RevampHttpMessageRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Psr/HttpMessage/RevampHttpMessageRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Psr\HttpMessage;
+
+use Ghostwriter\Revamp\Rule\Psr\HttpMessage\RevampHttpMessageRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampHttpMessageRector::class)]
+final class RevampHttpMessageRectorTest extends AbstractTestCase
+{
+}
