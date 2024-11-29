afbddfd6354de23a803d99c2b2d6d7e423b3a5a9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:44 -0600

    Add RevampLaminasMigrationRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasMigration/RevampLaminasMigrationRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasMigration;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasMigration\RevampLaminasMigrationRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasMigrationRector::class)]
+final class RevampLaminasMigrationRectorTest extends AbstractTestCase
+{
+}
