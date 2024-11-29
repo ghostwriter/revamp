40735a67fa654f4df5628a44e3d55d2f480b6426
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:27 -0600

    Add laminas-servicemanager-migration.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/laminas/laminas-servicemanager-migration.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules(
+        [\Ghostwriter\Revamp\Rule\Laminas\LaminasServicemanagerMigration\RevampLaminasServicemanagerMigrationRector::class]
+    )
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Laminas\LaminasServicemanagerMigration\LaminasServicemanagerMigrationSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
