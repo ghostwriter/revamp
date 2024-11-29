314f8b7e9d79d780ab055948910430857284f4cd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:48 -0600

    Add RevampLaminasProgressbarRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasProgressbar/RevampLaminasProgressbarRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasProgressbar;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasProgressbar\RevampLaminasProgressbarRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasProgressbarRector::class)]
+final class RevampLaminasProgressbarRectorTest extends AbstractTestCase
+{
+}
