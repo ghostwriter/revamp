207b49756cb7a9aa0bcaafcf4ca2f2e15623d605
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-29 07:43:29 -0600

    Update laminas-cache-storage-adapter-apcu.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/laminas/laminas-cache-storage-adapter-apcu.php
+++ config/laminas/laminas-cache-storage-adapter-apcu.php
@@ -6,7 +6,9 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withRules(
-        [\Ghostwriter\Revamp\Rule\Laminas\LaminasCacheStorageAdapterApcu\RevampLaminasCacheStorageAdapterApcuRector::class]
+        [
+            \Ghostwriter\Revamp\Rule\Laminas\LaminasCacheStorageAdapterApcu\RevampLaminasCacheStorageAdapterApcuRector::class,
+        ]
     )
 //    ->withSetProviders(
 //      // once this method is accepted, uncomment this line
