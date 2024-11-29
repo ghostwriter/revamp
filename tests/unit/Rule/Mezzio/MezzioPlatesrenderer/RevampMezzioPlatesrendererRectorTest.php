67c227be86dcdb3c970eedc6202cd35ef75fc215
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:53 -0600

    Add RevampMezzioPlatesrendererRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioPlatesrenderer/RevampMezzioPlatesrendererRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioPlatesrenderer;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioPlatesrenderer\RevampMezzioPlatesrendererRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioPlatesrendererRector::class)]
+final class RevampMezzioPlatesrendererRectorTest extends AbstractTestCase
+{
+}
