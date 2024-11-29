6b09bc3e7c505292ad7094f427ddcc4ae3fb5706
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:34 -0600

    Add RevampArmRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Ghostwriter/Arm/RevampArmRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Ghostwriter\Arm;
+
+use Ghostwriter\Revamp\Rule\Ghostwriter\Arm\RevampArmRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampArmRector::class)]
+final class RevampArmRectorTest extends AbstractTestCase
+{
+}
