cbfedd9e3aa3c78665064bc85840d75d5a36e638
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:38 -0600

    Add RevampLaminasCacheStorageAdapterSessionRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasCacheStorageAdapterSession/RevampLaminasCacheStorageAdapterSessionRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasCacheStorageAdapterSession;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasCacheStorageAdapterSession\RevampLaminasCacheStorageAdapterSessionRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasCacheStorageAdapterSessionRector::class)]
+final class RevampLaminasCacheStorageAdapterSessionRectorTest extends AbstractTestCase
+{
+}
