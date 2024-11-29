4968c57df47bc15b45ebf9da281f5a82749bfd39
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:52:33 -0600

    Add UseStatement.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/UseStatement.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Revamp;
+
+final readonly class UseStatement
+{
+    private string $alias;
+
+    private string $name;
+
+    public function __construct(
+        private string $full,
+    ) {
+        $parts = \explode('\\', $full);
+
+        $first = $parts[\array_key_first($parts)];
+
+        $last = $parts[\array_key_last($parts)];
+
+        $this->alias = (($first === $last) ? $last : $first . $last);
+
+        $this->name = $last;
+    }
+
+    public function alias(): string
+    {
+        return $this->alias;
+    }
+
+    public function full(): string
+    {
+        return $this->full;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+}
