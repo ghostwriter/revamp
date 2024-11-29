8302ea05de92eb39a8ed6d4f4d008a9dc8bee7e8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:21 -0600

    Add laminas-cache-storage-adapter-redis.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/laminas/laminas-cache-storage-adapter-redis.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules(
+        [\Ghostwriter\Revamp\Rule\Laminas\LaminasCacheStorageAdapterRedis\RevampLaminasCacheStorageAdapterRedisRector::class]
+    )
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Laminas\LaminasCacheStorageAdapterRedis\LaminasCacheStorageAdapterRedisSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
