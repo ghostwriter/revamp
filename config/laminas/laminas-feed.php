bcd78af7a79acbd6e16c5f54428e26cc55793a96
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:23 -0600

    Add laminas-feed.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/laminas/laminas-feed.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([\Ghostwriter\Revamp\Rule\Laminas\LaminasFeed\RevampLaminasFeedRector::class])
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Laminas\LaminasFeed\LaminasFeedSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
