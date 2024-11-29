072312ba8117797494d38344a5895f60f0f2d804
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:49 -0600

    Add RevampLaminasStdlibRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasStdlib/RevampLaminasStdlibRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasStdlib;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasStdlib\RevampLaminasStdlibRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasStdlibRector::class)]
+final class RevampLaminasStdlibRectorTest extends AbstractTestCase
+{
+}
