8035f9a3dbe28c733bb2ae408341cb93426f2072
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:32 -0600

    Add mezzio-tooling.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/mezzio/mezzio-tooling.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([\Ghostwriter\Revamp\Rule\Mezzio\MezzioTooling\RevampMezzioToolingRector::class])
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Mezzio\MezzioTooling\MezzioToolingSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
