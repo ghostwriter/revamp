3d6a451a033c4e83ad886bde39993397b864216e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:54:02 -0600

    Add RevampPsalmRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Vimeo/Psalm/RevampPsalmRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Vimeo\Psalm;
+
+use Ghostwriter\Revamp\Rule\Vimeo\Psalm\RevampPsalmRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampPsalmRector::class)]
+final class RevampPsalmRectorTest extends AbstractTestCase
+{
+}
