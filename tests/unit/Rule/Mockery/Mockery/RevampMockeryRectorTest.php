0b9c2fae287279f37a370b135411c2f7bb209e05
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:54 -0600

    Add RevampMockeryRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mockery/Mockery/RevampMockeryRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mockery\Mockery;
+
+use Ghostwriter\Revamp\Rule\Mockery\Mockery\RevampMockeryRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMockeryRector::class)]
+final class RevampMockeryRectorTest extends AbstractTestCase
+{
+}
