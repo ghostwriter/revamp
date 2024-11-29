b98fd37664b3a4b14c943c4171f3db19b89459d7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:51:34 -0600

    Add clock.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ config/psr/clock.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withRules([\Ghostwriter\Revamp\Rule\Psr\Clock\RevampClockRector::class])
+//    ->withSetProviders(
+//      // once this method is accepted, uncomment this line
+//        \Ghostwriter\Revamp\SetProvider\Psr\Clock\ClockSetProvider::class,
+//    )
+    ->withSets([
+        // your sets here
+    ]);
