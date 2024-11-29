aab9f3f456f856bd66db8e870600d9927f8f2a92
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-29 07:43:35 -0600

    Move config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Rule/PHPUnit/PHPUnit/TestThrowsThrowableDocCommentRector/config.php
+++ tests/fixture/Rule/PHPUnit/PHPUnit/TestThrowsThrowableDocCommentRector/config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Ghostwriter\Revamp\Rule\PHPUnit\TestThrowsThrowableDocCommentRector;
+use Ghostwriter\Revamp\Rule\PHPUnit\Phpunit\TestThrowsThrowableDocCommentRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()->withRules([TestThrowsThrowableDocCommentRector::class]);
