f14f3c3fe4cabccf7aa8ff5a0b9ca58866025c4e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:52:06 -0600

    Add ReplaceUnnecessaryDoubleQuotesRector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Rule/ReplaceUnnecessaryDoubleQuotesRector.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Revamp\Rule;
+
+use Ghostwriter\Revamp\AbstractRevampRector;
+use Ghostwriter\RevampTests\Rule\ReplaceUnnecessaryDoubleQuotesRectorTest;
+use PhpParser\Node;
+use PhpParser\Node\Scalar\String_;
+
+/**
+ * @see ReplaceUnnecessaryDoubleQuotesRectorTest
+ */
+final class ReplaceUnnecessaryDoubleQuotesRector extends AbstractRevampRector
+{
+    /**
+     * @return array<class-string<Node>>
+     */
+    #[\Override]
+    public function getNodeTypes(): array
+    {
+        return [String_::class];
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    #[\Override]
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node instanceof String_) {
+            return null;
+        }
+
+        if ($node->getAttribute('kind') !== $node::KIND_DOUBLE_QUOTED) {
+            return null;
+        }
+
+        if (\str_contains($node->value, '\\')) {
+            return null;
+        }
+
+        $node->setAttribute('kind', String_::KIND_SINGLE_QUOTED);
+
+        $node->setAttribute('origNode', null);
+
+        return $node;
+    }
+}
