a472b8a879102cf0466007fc5120a6d3a5e57239
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:53 -0600

    Add RevampMezzioSkeletonRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioSkeleton/RevampMezzioSkeletonRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioSkeleton;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioSkeleton\RevampMezzioSkeletonRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioSkeletonRector::class)]
+final class RevampMezzioSkeletonRectorTest extends AbstractTestCase
+{
+}
