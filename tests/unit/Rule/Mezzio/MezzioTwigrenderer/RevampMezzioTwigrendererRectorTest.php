9850505aa59732e4918b316f4bd3d520f2f93b47
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:54 -0600

    Add RevampMezzioTwigrendererRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Mezzio/MezzioTwigrenderer/RevampMezzioTwigrendererRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Mezzio\MezzioTwigrenderer;
+
+use Ghostwriter\Revamp\Rule\Mezzio\MezzioTwigrenderer\RevampMezzioTwigrendererRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampMezzioTwigrendererRector::class)]
+final class RevampMezzioTwigrendererRectorTest extends AbstractTestCase
+{
+}
