b216839782b248cff97288747eedc8ccb85d576f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-28 21:53:39 -0600

    Add RevampLaminasComposerAutoloadingRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Rule/Laminas/LaminasComposerAutoloading/RevampLaminasComposerAutoloadingRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\RevampTests\Rule\Laminas\LaminasComposerAutoloading;
+
+use Ghostwriter\Revamp\Rule\Laminas\LaminasComposerAutoloading\RevampLaminasComposerAutoloadingRector;
+use Ghostwriter\RevampTests\AbstractTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(RevampLaminasComposerAutoloadingRector::class)]
+final class RevampLaminasComposerAutoloadingRectorTest extends AbstractTestCase
+{
+}
