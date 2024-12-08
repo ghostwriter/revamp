b24189fabd86537f88649c61661b32ec5641c5da
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:26:33 -0600

    Move config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Php/Php53/ReplaceUnnecessaryDoubleQuotesRector/config.php
+++ tests/fixture/Package/Php/Php53/ReplaceUnnecessaryDoubleQuotesRector/config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Ghostwriter\Revamp\Rule\PHP\Core\ReplaceUnnecessaryDoubleQuotesRector;
+use Ghostwriter\Revamp\Package\Php\Php53\ReplaceUnnecessaryDoubleQuotesRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()->withRules([ReplaceUnnecessaryDoubleQuotesRector::class]);
