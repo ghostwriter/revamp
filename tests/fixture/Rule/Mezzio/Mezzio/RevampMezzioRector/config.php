9847fd5653eda148af0646a0277d6e64ce2452ad
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:27:21 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Rule/Mezzio/Mezzio/RevampMezzioRector/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([\Ghostwriter\Revamp\Rule\Mezzio\Mezzio\RevampMezzioRector::class]);
