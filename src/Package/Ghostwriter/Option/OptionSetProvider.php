f36bb7fc0cfad33bdeb586a901077d47a41a1cda
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:25:11 -0600

    Move OptionSetProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Package/Ghostwriter/Option/OptionSetProvider.php
+++ src/Package/Ghostwriter/Option/OptionSetProvider.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Revamp\SetProvider\Ghostwriter\Option;
+namespace Ghostwriter\Revamp\Package\Ghostwriter\Option;
 
+use Override;
 use Rector\Set\Contract\SetInterface;
 use Rector\Set\Contract\SetProviderInterface;
 use Rector\Set\ValueObject\ComposerTriggeredSet;
@@ -14,12 +15,12 @@ final readonly class OptionSetProvider implements SetProviderInterface
     /**
      * @return SetInterface[]
      */
-    #[\Override]
+    #[Override]
     public function provide(): array
     {
         return [
-            new ComposerTriggeredSet('group_name', 'package_name', '1.0', __DIR__ . 'set_file_path.php'),
-            new Set('group_name', 'set_name', __DIR__ . 'set_file_path.php'),
+            new ComposerTriggeredSet('Option', 'Option', '0', 'config/ghostwriter/option.php'),
+            new Set('Option', 'GhostwriterOptionOptionRector', 'config/ghostwriter/option.php'),
         ];
     }
 }
