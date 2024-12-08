dd0f533bddb88b6c334549081fa0d58dc3869c91
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:25:16 -0600

    Move Php53SetProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Package/Php/Php53/Php53SetProvider.php
+++ src/Package/Php/Php53/Php53SetProvider.php
@@ -2,24 +2,25 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Revamp\SetProvider\PHP\PHP56;
+namespace Ghostwriter\Revamp\Package\Php\Php53;
 
+use Override;
 use Rector\Set\Contract\SetInterface;
 use Rector\Set\Contract\SetProviderInterface;
 use Rector\Set\ValueObject\ComposerTriggeredSet;
 use Rector\Set\ValueObject\Set;
 
-final readonly class PHP56SetProvider implements SetProviderInterface
+final readonly class Php53SetProvider implements SetProviderInterface
 {
     /**
      * @return SetInterface[]
      */
-    #[\Override]
+    #[Override]
     public function provide(): array
     {
         return [
-            new ComposerTriggeredSet('group_name', 'package_name', '1.0', __DIR__ . 'set_file_path.php'),
-            new Set('group_name', 'set_name', __DIR__ . 'set_file_path.php'),
+            new ComposerTriggeredSet('Php53', 'Php53', '0', 'config/php/php53.php'),
+            new Set('Php53', 'PhpPhp53Php53Rector', 'config/php/php53.php'),
         ];
     }
 }
