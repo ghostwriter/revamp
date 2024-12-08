4415f43627ef71f6f8d1a559cce781390ff16b45
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:54 -0600

    Update php82.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/php/php82.php
+++ config/php/php82.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Php\Php82\Php82Rector;
+use Ghostwriter\Revamp\Package\Php\Php82\Php82SetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\PHP\PHP82\RevampPHP82Rector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\PHP\PHP82\PHP82SetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([Php82Rector::class])
+    // ->withSetProviders(Php82SetProvider::class)
+;
