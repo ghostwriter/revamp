c25d5f8520328c799f761fb3c6cfc10161224779
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:25:02 -0600

    Update UseStatementNotFoundException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/UseStatementNotFoundException.php
+++ src/Exception/UseStatementNotFoundException.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Ghostwriter\Revamp\Exception;
 
 use Ghostwriter\Revamp\ExceptionInterface;
+use InvalidArgumentException;
 
-final class UseStatementNotFoundException extends \InvalidArgumentException implements ExceptionInterface
+final class UseStatementNotFoundException extends InvalidArgumentException implements ExceptionInterface
 {
 }
