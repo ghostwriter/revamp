37e758aa3fa684d6dc4b8a32878a70341694d966
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:53 -0600

    Update php71.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/php/php71.php
+++ config/php/php71.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Php\Php71\Php71Rector;
+use Ghostwriter\Revamp\Package\Php\Php71\Php71SetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\PHP\PHP71\RevampPHP71Rector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\PHP\PHP71\PHP71SetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([Php71Rector::class])
+    // ->withSetProviders(Php71SetProvider::class)
+;
