eb402fdc3ed4f6b51c94cd04e3a7b7ad9b14594c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:55 -0600

    Update php84.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/php/php84.php
+++ config/php/php84.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Php\Php84\Php84Rector;
+use Ghostwriter\Revamp\Package\Php\Php84\Php84SetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\PHP\PHP84\RevampPHP84Rector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\PHP\PHP84\PHP84SetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([Php84Rector::class])
+    // ->withSetProviders(Php84SetProvider::class)
+;
