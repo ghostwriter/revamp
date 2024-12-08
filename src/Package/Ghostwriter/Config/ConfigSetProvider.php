30a6f26c8d307e9bb4601ad31093a7120348f74d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:25:06 -0600

    Move ConfigSetProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Package/Ghostwriter/Config/ConfigSetProvider.php
+++ src/Package/Ghostwriter/Config/ConfigSetProvider.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Revamp\SetProvider\Ghostwriter\Config;
+namespace Ghostwriter\Revamp\Package\Ghostwriter\Config;
 
+use Override;
 use Rector\Set\Contract\SetInterface;
 use Rector\Set\Contract\SetProviderInterface;
 use Rector\Set\ValueObject\ComposerTriggeredSet;
@@ -14,12 +15,12 @@ final readonly class ConfigSetProvider implements SetProviderInterface
     /**
      * @return SetInterface[]
      */
-    #[\Override]
+    #[Override]
     public function provide(): array
     {
         return [
-            new ComposerTriggeredSet('group_name', 'package_name', '1.0', __DIR__ . 'set_file_path.php'),
-            new Set('group_name', 'set_name', __DIR__ . 'set_file_path.php'),
+            new ComposerTriggeredSet('Config', 'Config', '0', 'config/ghostwriter/config.php'),
+            new Set('Config', 'GhostwriterConfigConfigRector', 'config/ghostwriter/config.php'),
         ];
     }
 }
