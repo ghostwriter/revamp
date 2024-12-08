cddcc9fb14728b1a1ec0408d7257fa83c49362e5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:28 -0600

    Update case-converter.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/case-converter.php
+++ config/ghostwriter/case-converter.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Ghostwriter\CaseConverter\CaseConverterRector;
+use Ghostwriter\Revamp\Package\Ghostwriter\CaseConverter\CaseConverterSetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\Ghostwriter\CaseConverter\RevampCaseConverterRector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\Ghostwriter\CaseConverter\CaseConverterSetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([CaseConverterRector::class])
+    // ->withSetProviders(CaseConverterSetProvider::class)
+;
