9f05060925c990f3803208ba173f5c0f7768578f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:26:31 -0600

    Move config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Mockery/Mockery/ShouldReceiveToAllowsRector/config.php
+++ tests/fixture/Package/Mockery/Mockery/ShouldReceiveToAllowsRector/config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Ghostwriter\Revamp\Rule\Mockery\ShouldReceiveToAllowsRector;
+use Ghostwriter\Revamp\Package\Mockery\Mockery\ShouldReceiveToAllowsRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()->withRules([ShouldReceiveToAllowsRector::class]);
