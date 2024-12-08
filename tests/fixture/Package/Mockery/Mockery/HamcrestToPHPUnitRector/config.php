1f61f2ac62958a7650fd0bf97509897b156d500b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:26:29 -0600

    Move config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Mockery/Mockery/HamcrestToPHPUnitRector/config.php
+++ tests/fixture/Package/Mockery/Mockery/HamcrestToPHPUnitRector/config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Ghostwriter\Revamp\Rule\Mockery\HamcrestToPHPUnitRector;
+use Ghostwriter\Revamp\Package\Mockery\Mockery\HamcrestToPHPUnitRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()->withRules([HamcrestToPHPUnitRector::class]);
