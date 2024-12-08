c1f891aec6b1546a3683c85bbf321a345e69cc0a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:27:13 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Rule/Laminas/LaminasPsr7Bridge/RevampLaminasPsr7BridgeRector/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules(
-    [\Ghostwriter\Revamp\Rule\Laminas\LaminasPsr7Bridge\RevampLaminasPsr7BridgeRector::class]
-);
