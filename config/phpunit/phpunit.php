de8cd015716cca9563ccff43a5fd2cf92759e500
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:55 -0600

    Update phpunit.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/phpunit/phpunit.php
+++ config/phpunit/phpunit.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Phpunit\Phpunit\TestThrowsThrowableDocCommentRector;
+use Ghostwriter\Revamp\Package\Phpunit\Phpunit\PhpunitSetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\PHPUnit\PHPUnit\RevampPHPUnitRector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\PHPUnit\PHPUnit\PHPUnitSetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([TestThrowsThrowableDocCommentRector::class])
+    // ->withSetProviders(PhpunitSetProvider::class)
+;
