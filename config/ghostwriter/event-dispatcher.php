7ad1806f2396efe1b539bfb943ab99780ae5a3e8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:29 -0600

    Update event-dispatcher.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/event-dispatcher.php
+++ config/ghostwriter/event-dispatcher.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Ghostwriter\EventDispatcher\EventDispatcherRector;
+use Ghostwriter\Revamp\Package\Ghostwriter\EventDispatcher\EventDispatcherSetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\Ghostwriter\EventDispatcher\RevampEventDispatcherRector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\Ghostwriter\EventDispatcher\EventDispatcherSetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([EventDispatcherRector::class])
+    // ->withSetProviders(EventDispatcherSetProvider::class)
+;
