a0867801b475f05c6ebdd139b6d731c867c0c5a2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:30 -0600

    Update phormat.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/phormat.php
+++ config/ghostwriter/phormat.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Ghostwriter\Phormat\PhormatRector;
+use Ghostwriter\Revamp\Package\Ghostwriter\Phormat\PhormatSetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\Ghostwriter\Phormat\RevampPhormatRector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\Ghostwriter\Phormat\PhormatSetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([PhormatRector::class])
+    // ->withSetProviders(PhormatSetProvider::class)
+;
