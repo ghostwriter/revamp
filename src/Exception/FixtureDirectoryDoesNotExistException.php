17ba040f2f9edb3bade0c9c41d06d8b9ec2d3370
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:25:01 -0600

    Update FixtureDirectoryDoesNotExistException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FixtureDirectoryDoesNotExistException.php
+++ src/Exception/FixtureDirectoryDoesNotExistException.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Ghostwriter\Revamp\Exception;
 
 use Ghostwriter\Revamp\ExceptionInterface;
+use InvalidArgumentException;
 
-final class FixtureDirectoryDoesNotExistException extends \InvalidArgumentException implements ExceptionInterface
+final class FixtureDirectoryDoesNotExistException extends InvalidArgumentException implements ExceptionInterface
 {
 }
