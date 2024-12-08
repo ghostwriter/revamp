5d8eb7ed6269a0aa12d1c790466b02d4654bb4d0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:25:11 -0600

    Move PhormatSetProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Package/Ghostwriter/Phormat/PhormatSetProvider.php
+++ src/Package/Ghostwriter/Phormat/PhormatSetProvider.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Revamp\SetProvider\Ghostwriter\Phormat;
+namespace Ghostwriter\Revamp\Package\Ghostwriter\Phormat;
 
+use Override;
 use Rector\Set\Contract\SetInterface;
 use Rector\Set\Contract\SetProviderInterface;
 use Rector\Set\ValueObject\ComposerTriggeredSet;
@@ -14,12 +15,12 @@ final readonly class PhormatSetProvider implements SetProviderInterface
     /**
      * @return SetInterface[]
      */
-    #[\Override]
+    #[Override]
     public function provide(): array
     {
         return [
-            new ComposerTriggeredSet('group_name', 'package_name', '1.0', __DIR__ . 'set_file_path.php'),
-            new Set('group_name', 'set_name', __DIR__ . 'set_file_path.php'),
+            new ComposerTriggeredSet('Phormat', 'Phormat', '0', 'config/ghostwriter/phormat.php'),
+            new Set('Phormat', 'GhostwriterPhormatPhormatRector', 'config/ghostwriter/phormat.php'),
         ];
     }
 }
