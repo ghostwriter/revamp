cf5baf4ef453423c088fe04e3ca6d7fe0399a441
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:53 -0600

    Update php74.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/php/php74.php
+++ config/php/php74.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Php\Php74\Php74Rector;
+use Ghostwriter\Revamp\Package\Php\Php74\Php74SetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\PHP\PHP74\RevampPHP74Rector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\PHP\PHP74\PHP74SetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([Php74Rector::class])
+    // ->withSetProviders(Php74SetProvider::class)
+;
