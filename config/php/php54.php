b2f5e201f7f7db0b3d9d09ebaff63eacdc7ccef2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:52 -0600

    Update php54.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/php/php54.php
+++ config/php/php54.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Php\Php54\Php54Rector;
+use Ghostwriter\Revamp\Package\Php\Php54\Php54SetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\PHP\PHP54\RevampPHP54Rector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\PHP\PHP54\PHP54SetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([Php54Rector::class])
+    // ->withSetProviders(Php54SetProvider::class)
+;
