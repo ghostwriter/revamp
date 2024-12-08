ffa61c67d309653818cefec791d4100a648b6edd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:25:14 -0600

    Move MockerySetProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Package/Mockery/Mockery/MockerySetProvider.php
+++ src/Package/Mockery/Mockery/MockerySetProvider.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Revamp\SetProvider\Mockery\Mockery;
+namespace Ghostwriter\Revamp\Package\Mockery\Mockery;
 
+use Override;
 use Rector\Set\Contract\SetInterface;
 use Rector\Set\Contract\SetProviderInterface;
 use Rector\Set\ValueObject\ComposerTriggeredSet;
@@ -14,12 +15,12 @@ final readonly class MockerySetProvider implements SetProviderInterface
     /**
      * @return SetInterface[]
      */
-    #[\Override]
+    #[Override]
     public function provide(): array
     {
         return [
-            new ComposerTriggeredSet('group_name', 'package_name', '1.0', __DIR__ . 'set_file_path.php'),
-            new Set('group_name', 'set_name', __DIR__ . 'set_file_path.php'),
+            new ComposerTriggeredSet('Mockery', 'Mockery', '0', 'config/mockery/mockery.php'),
+            new Set('Mockery', 'MockeryMockeryMockeryRector', 'config/mockery/mockery.php'),
         ];
     }
 }
