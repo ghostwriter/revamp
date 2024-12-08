a2c6a4343e1eee4743c5149b4c2ef965463f8f4e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:27:20 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Rule/Laminas/LaminasXml2Json/RevampLaminasXml2JsonRector/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules(
-    [\Ghostwriter\Revamp\Rule\Laminas\LaminasXml2Json\RevampLaminasXml2JsonRector::class]
-);
