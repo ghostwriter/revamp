558f78d4cda653926c444cd8795450eee24fb90c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:26:37 -0600

    Move config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Php/Php80/RemoveDuplicateAttributesRector/config.php
+++ tests/fixture/Package/Php/Php80/RemoveDuplicateAttributesRector/config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Ghostwriter\Revamp\Rule\PHP\PHP80\RemoveDuplicateAttributesRector;
+use Ghostwriter\Revamp\Package\Php\Php80\RemoveDuplicateAttributesRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()->withRules([RemoveDuplicateAttributesRector::class]);
