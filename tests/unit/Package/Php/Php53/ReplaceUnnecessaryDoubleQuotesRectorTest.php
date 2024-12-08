1dfa2f65d3ffe844f828bf9956667959ae4531a6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:27:48 -0600

    Move ReplaceUnnecessaryDoubleQuotesRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Php/Php53/ReplaceUnnecessaryDoubleQuotesRectorTest.php
+++ tests/unit/Package/Php/Php53/ReplaceUnnecessaryDoubleQuotesRectorTest.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\RevampTests\Rule\PHP\Core;
+namespace Tests\Unit\Package\Php\Php53;
 
-use Ghostwriter\Revamp\Rule\PHP\Core\ReplaceUnnecessaryDoubleQuotesRector;
-use Ghostwriter\RevampTests\AbstractTestCase;
+use Ghostwriter\Revamp\Package\Php\Php53\ReplaceUnnecessaryDoubleQuotesRector;
 use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(ReplaceUnnecessaryDoubleQuotesRector::class)]
 final class ReplaceUnnecessaryDoubleQuotesRectorTest extends AbstractTestCase
