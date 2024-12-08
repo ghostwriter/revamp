1cb4caa1585dbc59d0b5601f9f1b5b8ca884dc3f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:26:34 -0600

    Move config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Php/Php53/SortClassLikeStatementsAlphabeticallyRector/config.php
+++ tests/fixture/Package/Php/Php53/SortClassLikeStatementsAlphabeticallyRector/config.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Ghostwriter\Revamp\Rule\PHP\Core\SortClassLikeStatementsAlphabeticallyRector;
+use Ghostwriter\Revamp\Package\Php\Php53\SortClassLikeStatementsAlphabeticallyRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()->withRules([SortClassLikeStatementsAlphabeticallyRector::class]);
