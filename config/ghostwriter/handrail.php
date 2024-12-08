e951e41f65bdbf62baad862bbaa9e4503bf3a4e6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-08 15:24:30 -0600

    Update handrail.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/handrail.php
+++ config/ghostwriter/handrail.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Revamp\Package\Ghostwriter\Handrail\HandrailRector;
+use Ghostwriter\Revamp\Package\Ghostwriter\Handrail\HandrailSetProvider;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withRules([\Ghostwriter\Revamp\Rule\Ghostwriter\Handrail\RevampHandrailRector::class])
-//    ->withSetProviders(
-//      // once this method is accepted, uncomment this line
-//        \Ghostwriter\Revamp\SetProvider\Ghostwriter\Handrail\HandrailSetProvider::class,
-//    )
-    ->withSets([
-        // your sets here
-    ]);
+    ->withRules([HandrailRector::class])
+    // ->withSetProviders(HandrailSetProvider::class)
+;
