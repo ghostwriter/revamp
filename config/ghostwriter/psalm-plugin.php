9ed503884c680acfa23e4bda4f01b0908b68edb3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:09 -0600

    Update psalm-plugin.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/psalm-plugin.php
+++ config/ghostwriter/psalm-plugin.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\PsalmPlugin\PsalmPluginRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\PsalmPlugin\PsalmPluginSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([PsalmPluginRector::class])
-    // ->withSetProviders(PsalmPluginSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/psalm-plugin')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/psalm-plugin', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(PsalmPluginRector::class);
+};
