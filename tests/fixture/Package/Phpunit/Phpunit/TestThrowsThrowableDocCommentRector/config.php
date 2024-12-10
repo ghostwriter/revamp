a0f7b6c4d307b032a138cf461a7efd9ebd380871
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:57 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Phpunit/Phpunit/TestThrowsThrowableDocCommentRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Phpunit\Phpunit\TestThrowsThrowableDocCommentRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([TestThrowsThrowableDocCommentRector::class]);
