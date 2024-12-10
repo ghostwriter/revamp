fc6b6d1794593f90370b935b19acfe70af395fae
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:05 -0600

    Move SortClassLikeStatementsAlphabeticallyRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Php/Php/SortClassLikeStatementsAlphabeticallyRectorTest.php
+++ tests/unit/Package/Php/Php/SortClassLikeStatementsAlphabeticallyRectorTest.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit\Package\Php\Php53;
+namespace Tests\Unit\Package\Php\Php;
 
-use Ghostwriter\Revamp\Package\Php\Php53\SortClassLikeStatementsAlphabeticallyRector;
+use Ghostwriter\Revamp\Package\Php\Php\SortClassLikeStatementsAlphabeticallyRector;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
