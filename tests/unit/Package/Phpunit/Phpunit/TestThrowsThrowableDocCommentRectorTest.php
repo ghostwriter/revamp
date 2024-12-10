01d19d6165b6f367a573c651ed0aee8fa9f5ccca
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:08 -0600

    Update TestThrowsThrowableDocCommentRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Phpunit/Phpunit/TestThrowsThrowableDocCommentRectorTest.php
+++ tests/unit/Package/Phpunit/Phpunit/TestThrowsThrowableDocCommentRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Phpunit\Phpunit;
 
 use Ghostwriter\Revamp\Package\Phpunit\Phpunit\TestThrowsThrowableDocCommentRector;
-use Ghostwriter\Revamp\Package\Phpunit\Phpunit\PhpunitSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(TestThrowsThrowableDocCommentRector::class)]
-#[CoversClass(PhpunitSetProvider::class)]
 final class TestThrowsThrowableDocCommentRectorTest extends AbstractTestCase
 {
 }
