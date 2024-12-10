42f601edc95799144722907780551e6f141efcf8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:09 -0600

    Update shell.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/shell.php
+++ config/ghostwriter/shell.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\Shell\ShellRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Shell\ShellSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([ShellRector::class])
-    // ->withSetProviders(ShellSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/shell')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/shell', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(ShellRector::class);
+};
