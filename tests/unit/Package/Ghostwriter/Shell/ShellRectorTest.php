baa318401630929fbef5561a9b7df3f9320f7649
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:03 -0600

    Update ShellRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Shell/ShellRectorTest.php
+++ tests/unit/Package/Ghostwriter/Shell/ShellRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Shell;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Shell\ShellRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Shell\ShellSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(ShellRector::class)]
-#[CoversClass(ShellSetProvider::class)]
 final class ShellRectorTest extends AbstractTestCase
 {
 }
