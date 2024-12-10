374bfbf5245566028fe5b02dbc486e4ed6ee04d1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:37 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Ghostwriter/Collection/CollectionRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Ghostwriter\Collection\CollectionRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([CollectionRector::class]);
