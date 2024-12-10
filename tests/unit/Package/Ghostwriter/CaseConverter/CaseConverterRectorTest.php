cb6f424a89506c36dcf484396bea26ca35788e95
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:58 -0600

    Update CaseConverterRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/CaseConverter/CaseConverterRectorTest.php
+++ tests/unit/Package/Ghostwriter/CaseConverter/CaseConverterRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\CaseConverter;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\CaseConverter\CaseConverterRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\CaseConverter\CaseConverterSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(CaseConverterRector::class)]
-#[CoversClass(CaseConverterSetProvider::class)]
 final class CaseConverterRectorTest extends AbstractTestCase
 {
 }
