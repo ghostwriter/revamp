ce36ee6b63fa1bc96c3f0101973164d3dde46710
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:02 -0600

    Update PhptRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Phpt/PhptRectorTest.php
+++ tests/unit/Package/Ghostwriter/Phpt/PhptRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Phpt;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Phpt\PhptRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Phpt\PhptSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(PhptRector::class)]
-#[CoversClass(PhptSetProvider::class)]
 final class PhptRectorTest extends AbstractTestCase
 {
 }
