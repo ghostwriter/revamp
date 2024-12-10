9b2def7431e0cfd6b002e79c6ff44843cabb20d8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:02 -0600

    Update JsonRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Json/JsonRectorTest.php
+++ tests/unit/Package/Ghostwriter/Json/JsonRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Json;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Json\JsonRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Json\JsonSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(JsonRector::class)]
-#[CoversClass(JsonSetProvider::class)]
 final class JsonRectorTest extends AbstractTestCase
 {
 }
