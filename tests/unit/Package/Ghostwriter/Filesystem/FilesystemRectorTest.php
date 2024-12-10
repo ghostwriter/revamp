8cfddfc65e9233bf3fa3655e9121bc884633ec0c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:01 -0600

    Update FilesystemRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Filesystem/FilesystemRectorTest.php
+++ tests/unit/Package/Ghostwriter/Filesystem/FilesystemRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Filesystem;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Filesystem\FilesystemRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Filesystem\FilesystemSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(FilesystemRector::class)]
-#[CoversClass(FilesystemSetProvider::class)]
 final class FilesystemRectorTest extends AbstractTestCase
 {
 }
