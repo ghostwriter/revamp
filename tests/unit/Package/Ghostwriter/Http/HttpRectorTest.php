e88c9056fae0d742083c1837cf40529ab02ac2e1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:01 -0600

    Update HttpRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Http/HttpRectorTest.php
+++ tests/unit/Package/Ghostwriter/Http/HttpRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Http;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Http\HttpRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Http\HttpSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(HttpRector::class)]
-#[CoversClass(HttpSetProvider::class)]
 final class HttpRectorTest extends AbstractTestCase
 {
 }
