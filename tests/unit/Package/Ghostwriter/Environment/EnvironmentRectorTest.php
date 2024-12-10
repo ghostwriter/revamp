c957ce096eedbe1edb8cd1b9c31e53345be2bda7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:00 -0600

    Update EnvironmentRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Environment/EnvironmentRectorTest.php
+++ tests/unit/Package/Ghostwriter/Environment/EnvironmentRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Environment;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Environment\EnvironmentRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Environment\EnvironmentSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(EnvironmentRector::class)]
-#[CoversClass(EnvironmentSetProvider::class)]
 final class EnvironmentRectorTest extends AbstractTestCase
 {
 }
