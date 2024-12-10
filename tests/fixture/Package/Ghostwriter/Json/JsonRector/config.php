158d598146ff34e9d753d57c2e06ab3c0c4d36ec
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:40 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Ghostwriter/Json/JsonRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Ghostwriter\Json\JsonRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([JsonRector::class]);
