461fc5c73038ff4f60b94610e6ee12a4a2cea18e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:00 -0600

    Update EventDispatcherRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/EventDispatcher/EventDispatcherRectorTest.php
+++ tests/unit/Package/Ghostwriter/EventDispatcher/EventDispatcherRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\EventDispatcher;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\EventDispatcher\EventDispatcherRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\EventDispatcher\EventDispatcherSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(EventDispatcherRector::class)]
-#[CoversClass(EventDispatcherSetProvider::class)]
 final class EventDispatcherRectorTest extends AbstractTestCase
 {
 }
