b365bb55c0670db556cfd66413eca00ff68c977c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:03 -0600

    Update ResultRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Result/ResultRectorTest.php
+++ tests/unit/Package/Ghostwriter/Result/ResultRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Result;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Result\ResultRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Result\ResultSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(ResultRector::class)]
-#[CoversClass(ResultSetProvider::class)]
 final class ResultRectorTest extends AbstractTestCase
 {
 }
