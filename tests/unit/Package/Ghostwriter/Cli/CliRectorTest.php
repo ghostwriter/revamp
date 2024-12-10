f80623937a417de474c82520f95fc181c373efa1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:58 -0600

    Update CliRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Cli/CliRectorTest.php
+++ tests/unit/Package/Ghostwriter/Cli/CliRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Cli;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Cli\CliRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Cli\CliSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(CliRector::class)]
-#[CoversClass(CliSetProvider::class)]
 final class CliRectorTest extends AbstractTestCase
 {
 }
