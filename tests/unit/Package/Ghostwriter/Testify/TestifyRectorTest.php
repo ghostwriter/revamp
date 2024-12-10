23c9453aba7dfa744b05c7813a4d79c340554117
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:03 -0600

    Update TestifyRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Testify/TestifyRectorTest.php
+++ tests/unit/Package/Ghostwriter/Testify/TestifyRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Testify;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Testify\TestifyRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Testify\TestifySetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(TestifyRector::class)]
-#[CoversClass(TestifySetProvider::class)]
 final class TestifyRectorTest extends AbstractTestCase
 {
 }
