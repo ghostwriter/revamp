466ed51df278b81b090130739722283243429607
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:15 -0600

    Update psalm.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/vimeo/psalm.php
+++ config/vimeo/psalm.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Vimeo\Psalm\PsalmRector;
-use Ghostwriter\Revamp\Package\Vimeo\Psalm\PsalmSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([PsalmRector::class])
-    // ->withSetProviders(PsalmSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('vimeo/psalm')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'vimeo/psalm', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(PsalmRector::class);
+};
