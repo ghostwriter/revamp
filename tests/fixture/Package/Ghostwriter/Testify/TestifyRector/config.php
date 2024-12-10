29f54e5981f62973bb5e44d6b475d05d3e849b84
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:43 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Ghostwriter/Testify/TestifyRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Ghostwriter\Testify\TestifyRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([TestifyRector::class]);
