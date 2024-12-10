fb4f74db5aac7f3c2870ce42f4d2d83980ef3a63
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:56 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Php/Php80/Php80Rector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Php\Php80\Php80Rector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([Php80Rector::class]);
