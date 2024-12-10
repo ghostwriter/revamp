9d36e86a32f08b7e9fffe91f18def0d351e2bf3d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:03 -0600

    Update case-converter.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/case-converter.php
+++ config/ghostwriter/case-converter.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\CaseConverter\CaseConverterRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\CaseConverter\CaseConverterSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([CaseConverterRector::class])
-    // ->withSetProviders(CaseConverterSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/case-converter')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/case-converter', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(CaseConverterRector::class);
+};
