1088b0e90c4f6a7e119391ba2998d881303b0ecb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:02 -0600

    Update arm.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/arm.php
+++ config/ghostwriter/arm.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\Arm\ArmRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Arm\ArmSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([ArmRector::class])
-    // ->withSetProviders(ArmSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/arm')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/arm', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(ArmRector::class);
+};
