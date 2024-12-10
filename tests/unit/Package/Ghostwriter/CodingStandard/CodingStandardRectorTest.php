0ef0bf290af75b3a22105ec5c6e1e049bdaa9624
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:59 -0600

    Update CodingStandardRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/CodingStandard/CodingStandardRectorTest.php
+++ tests/unit/Package/Ghostwriter/CodingStandard/CodingStandardRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\CodingStandard;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\CodingStandard\CodingStandardRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\CodingStandard\CodingStandardSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(CodingStandardRector::class)]
-#[CoversClass(CodingStandardSetProvider::class)]
 final class CodingStandardRectorTest extends AbstractTestCase
 {
 }
