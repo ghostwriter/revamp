238973b608d15df180a14beb012f324ac28e458a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:05 -0600

    Update compliance.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/compliance.php
+++ config/ghostwriter/compliance.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\Compliance\ComplianceRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Compliance\ComplianceSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([ComplianceRector::class])
-    // ->withSetProviders(ComplianceSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/compliance')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/compliance', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(ComplianceRector::class);
+};
