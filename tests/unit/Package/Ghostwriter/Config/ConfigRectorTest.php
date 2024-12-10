328ca8827558cacf00d03b9b92d078487500ee49
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:00 -0600

    Update ConfigRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Config/ConfigRectorTest.php
+++ tests/unit/Package/Ghostwriter/Config/ConfigRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Config;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Config\ConfigRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Config\ConfigSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(ConfigRector::class)]
-#[CoversClass(ConfigSetProvider::class)]
 final class ConfigRectorTest extends AbstractTestCase
 {
 }
