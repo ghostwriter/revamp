9a2e26bf7ad727d70db5e496343016acd33af74f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:03 -0600

    Update PsalmPluginRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/PsalmPlugin/PsalmPluginRectorTest.php
+++ tests/unit/Package/Ghostwriter/PsalmPlugin/PsalmPluginRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\PsalmPlugin;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\PsalmPlugin\PsalmPluginRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\PsalmPlugin\PsalmPluginSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(PsalmPluginRector::class)]
-#[CoversClass(PsalmPluginSetProvider::class)]
 final class PsalmPluginRectorTest extends AbstractTestCase
 {
 }
