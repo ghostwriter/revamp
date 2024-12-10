70ab177451008f23e0a07792560b70380174c92d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:08 -0600

    Update Php85RectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Php/Php85/Php85RectorTest.php
+++ tests/unit/Package/Php/Php85/Php85RectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Php\Php85;
 
 use Ghostwriter\Revamp\Package\Php\Php85\Php85Rector;
-use Ghostwriter\Revamp\Package\Php\Php85\Php85SetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(Php85Rector::class)]
-#[CoversClass(Php85SetProvider::class)]
 final class Php85RectorTest extends AbstractTestCase
 {
 }
