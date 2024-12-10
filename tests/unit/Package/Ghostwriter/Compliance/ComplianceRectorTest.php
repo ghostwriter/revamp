483b4ef85f694f20936c617d03248d778fe6f814
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:59 -0600

    Update ComplianceRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Compliance/ComplianceRectorTest.php
+++ tests/unit/Package/Ghostwriter/Compliance/ComplianceRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Compliance;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Compliance\ComplianceRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Compliance\ComplianceSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(ComplianceRector::class)]
-#[CoversClass(ComplianceSetProvider::class)]
 final class ComplianceRectorTest extends AbstractTestCase
 {
 }
