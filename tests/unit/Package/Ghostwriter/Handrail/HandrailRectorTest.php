6c73456c3f6468d5293afcdd663c23ea86853c7d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:01 -0600

    Update HandrailRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Handrail/HandrailRectorTest.php
+++ tests/unit/Package/Ghostwriter/Handrail/HandrailRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Handrail;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Handrail\HandrailRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Handrail\HandrailSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(HandrailRector::class)]
-#[CoversClass(HandrailSetProvider::class)]
 final class HandrailRectorTest extends AbstractTestCase
 {
 }
