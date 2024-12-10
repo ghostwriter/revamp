937c7c9500cd5d7a32941859be5fe233db5f5524
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:41 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Ghostwriter/Phpt/PhptRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Ghostwriter\Phpt\PhptRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([PhptRector::class]);
