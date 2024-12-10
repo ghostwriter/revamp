de45b5acb4a14a405c862f5ff499341e6a244e57
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:04 -0600

    Add ParserFactoryCreateMethodRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Package/Nikic/PhpParser/ParserFactoryCreateMethodRectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Package\Nikic\PhpParser;
+
+use Ghostwriter\Revamp\Package\Nikic\PhpParser\ParserFactoryCreateMethodRector;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Unit\AbstractTestCase;
+
+#[CoversClass(ParserFactoryCreateMethodRector::class)]
+final class ParserFactoryCreateMethodRectorTest extends AbstractTestCase
+{
+}
