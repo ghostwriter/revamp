6b6dbfc37c08a258a5c2701d76b4760d9b7eb3d9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:07 -0600

    Update filesystem.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/filesystem.php
+++ config/ghostwriter/filesystem.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\Filesystem\FilesystemRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Filesystem\FilesystemSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([FilesystemRector::class])
-    // ->withSetProviders(FilesystemSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/filesystem')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/filesystem', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(FilesystemRector::class);
+};
