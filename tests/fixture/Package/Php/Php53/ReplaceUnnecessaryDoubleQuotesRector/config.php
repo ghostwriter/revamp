fd5de43ce97b346cd87cf3539410e777ff6750a8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:54 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Php/Php53/ReplaceUnnecessaryDoubleQuotesRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Php\Php53\ReplaceUnnecessaryDoubleQuotesRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([ReplaceUnnecessaryDoubleQuotesRector::class]);
