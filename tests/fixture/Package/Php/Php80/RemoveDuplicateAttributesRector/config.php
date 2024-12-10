88561ca286db3193635b663f4b75167f729b28dd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:56 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Php/Php80/RemoveDuplicateAttributesRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Php\Php80\RemoveDuplicateAttributesRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([RemoveDuplicateAttributesRector::class]);
