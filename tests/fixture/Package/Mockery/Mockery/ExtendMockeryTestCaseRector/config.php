ead8cfd72513dcd57543aa5d50244764b0a2113f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:43 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Mockery/Mockery/ExtendMockeryTestCaseRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Mockery\Mockery\ExtendMockeryTestCaseRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([ExtendMockeryTestCaseRector::class]);
