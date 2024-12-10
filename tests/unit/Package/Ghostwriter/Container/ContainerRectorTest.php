635adf024adb425240a096828d33923419b043f1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:00 -0600

    Update ContainerRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Container/ContainerRectorTest.php
+++ tests/unit/Package/Ghostwriter/Container/ContainerRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Container;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Container\ContainerRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Container\ContainerSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(ContainerRector::class)]
-#[CoversClass(ContainerSetProvider::class)]
 final class ContainerRectorTest extends AbstractTestCase
 {
 }
