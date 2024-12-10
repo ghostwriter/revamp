3ad865422b89c0343088a5949ce6ee96ca0882de
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:07 -0600

    Update event-dispatcher.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/event-dispatcher.php
+++ config/ghostwriter/event-dispatcher.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\EventDispatcher\EventDispatcherRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\EventDispatcher\EventDispatcherSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([EventDispatcherRector::class])
-    // ->withSetProviders(EventDispatcherSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/event-dispatcher')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/event-dispatcher', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(EventDispatcherRector::class);
+};
