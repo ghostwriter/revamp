84e6f06669ed90e3e793d4171d6c65c9690d5ac4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:04 -0600

    Update PHPUnitToMockeryRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Mockery/Mockery/PHPUnitToMockeryRectorTest.php
+++ tests/unit/Package/Mockery/Mockery/PHPUnitToMockeryRectorTest.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Package\Mockery\Mockery;
 
-use Ghostwriter\Revamp\Package\Mockery\Mockery\PHPUnitToMockeryRector;
+use Ghostwriter\Revamp\Package\Mockery\Mockery\PhpunitToMockeryRector;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
-#[CoversClass(PHPUnitToMockeryRector::class)]
-final class PHPUnitToMockeryRectorTest extends AbstractTestCase
+#[CoversClass(PhpunitToMockeryRector::class)]
+final class PhpunitToMockeryRectorTest extends AbstractTestCase
 {
 }
