399b6949f33f9982c1fdefb2ce329fdf1cc33c39
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:03 -0600

    Update coding-standard.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/coding-standard.php
+++ config/ghostwriter/coding-standard.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\CodingStandard\CodingStandardRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\CodingStandard\CodingStandardSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([CodingStandardRector::class])
-    // ->withSetProviders(CodingStandardSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/coding-standard')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/coding-standard', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(CodingStandardRector::class);
+};
