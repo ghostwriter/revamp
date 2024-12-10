c39723e500b7f47d98ba49943bdd25ca478c8b92
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:03 -0600

    Update UuidRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Uuid/UuidRectorTest.php
+++ tests/unit/Package/Ghostwriter/Uuid/UuidRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Uuid;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Uuid\UuidRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Uuid\UuidSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(UuidRector::class)]
-#[CoversClass(UuidSetProvider::class)]
 final class UuidRectorTest extends AbstractTestCase
 {
 }
