3a5201aca48c11443f51e875efd1e054de52afab
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:09 -0600

    Update result.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/result.php
+++ config/ghostwriter/result.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\Result\ResultRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Result\ResultSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([ResultRector::class])
-    // ->withSetProviders(ResultSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/result')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/result', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(ResultRector::class);
+};
