d46f7060a473e4d16a01a9cfd876b2d363145aba
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:23:02 -0600

    Update OptionRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Option/OptionRectorTest.php
+++ tests/unit/Package/Ghostwriter/Option/OptionRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Option;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Option\OptionRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Option\OptionSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(OptionRector::class)]
-#[CoversClass(OptionSetProvider::class)]
 final class OptionRectorTest extends AbstractTestCase
 {
 }
