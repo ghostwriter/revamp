64ee5ff827c4d319943385b15df5920be7a13128
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:07 -0600

    Update environment.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- config/ghostwriter/environment.php
+++ config/ghostwriter/environment.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Ghostwriter\Revamp\Package\Ghostwriter\Environment\EnvironmentRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Environment\EnvironmentSetProvider;
 use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
-    ->withRules([EnvironmentRector::class])
-    // ->withSetProviders(EnvironmentSetProvider::class)
-;
+return static function (RectorConfig $rectorConfig): void {
+    if (! InstalledVersions::isInstalled('ghostwriter/environment')) {
+        return;
+    }
+
+    if (! InstalledVersions::satisfies(new VersionParser(), 'ghostwriter/environment', '*')) {
+        return;
+    }
+
+    $rectorConfig->rule(EnvironmentRector::class);
+};
