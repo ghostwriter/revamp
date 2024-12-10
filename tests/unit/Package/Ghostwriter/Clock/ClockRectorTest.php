697b67ba89ddb282bc45343c8d89200ce4ae378b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:59 -0600

    Update ClockRectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Package/Ghostwriter/Clock/ClockRectorTest.php
+++ tests/unit/Package/Ghostwriter/Clock/ClockRectorTest.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Tests\Unit\Package\Ghostwriter\Clock;
 
 use Ghostwriter\Revamp\Package\Ghostwriter\Clock\ClockRector;
-use Ghostwriter\Revamp\Package\Ghostwriter\Clock\ClockSetProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(ClockRector::class)]
-#[CoversClass(ClockSetProvider::class)]
 final class ClockRectorTest extends AbstractTestCase
 {
 }
