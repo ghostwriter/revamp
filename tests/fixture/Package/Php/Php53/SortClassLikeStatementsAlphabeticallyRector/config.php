9a8779041c219b3dc92eebc961c9b30e1f02ddf3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-09 23:22:55 -0600

    Remove config.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/fixture/Package/Php/Php53/SortClassLikeStatementsAlphabeticallyRector/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Ghostwriter\Revamp\Package\Php\Php53\SortClassLikeStatementsAlphabeticallyRector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([SortClassLikeStatementsAlphabeticallyRector::class]);
