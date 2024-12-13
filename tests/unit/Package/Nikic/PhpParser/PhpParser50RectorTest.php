7436f84e4f622eab17ca2b845b8b082a7cf52fa6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-13 11:57:50 -0600

    Add PhpParser50RectorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Package/Nikic/PhpParser/PhpParser50RectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Package\Nikic\PhpParser;
+
+use Ghostwriter\Revamp\Package\Nikic\PhpParser\PhpParser50Rector;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\Unit\AbstractTestCase;
+
+#[CoversClass(PhpParser50Rector::class)]
+final class PhpParser50RectorTest extends AbstractTestCase
+{
+}
